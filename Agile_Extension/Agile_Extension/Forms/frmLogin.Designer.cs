4729f573b0a71ca34790510a4de60207a5265fcf
henro47 <53427025+henro47@users.noreply.github.com>
2021-09-25 23:06:46 +0200

    GIT LFS init

---
+++ Agile_Extension/Agile_Extension/Forms/frmLogin.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace Agile_Extension.Forms
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroSetControlBox1 = new MetroSet_UI.Controls.MetroSetControlBox();
+            this.lblUsername = new MetroSet_UI.Controls.MetroSetTextBox();
+            this.metroSetLabel1 = new MetroSet_UI.Controls.MetroSetLabel();
+            this.metroSetLabel2 = new MetroSet_UI.Controls.MetroSetLabel();
+            this.lblPassword = new MetroSet_UI.Controls.MetroSetTextBox();
+            this.btnLogin = new MetroSet_UI.Controls.MetroSetButton();
+            this.metroSetLink1 = new MetroSet_UI.Controls.MetroSetLink();
+            this.styleManager1 = new MetroSet_UI.StyleManager();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::Agile_Extension.Properties.Resources.Fragile_LOGO;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // metroSetControlBox1
+            // 
+            this.metroSetControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroSetControlBox1.CloseHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.metroSetControlBox1.CloseHoverForeColor = System.Drawing.Color.White;
+            this.metroSetControlBox1.CloseNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.DisabledForeColor = System.Drawing.Color.DimGray;
+            this.metroSetControlBox1.Location = new System.Drawing.Point(187, 8);
+            this.metroSetControlBox1.MaximizeBox = true;
+            this.metroSetControlBox1.MaximizeHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.metroSetControlBox1.MaximizeHoverForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MaximizeNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MinimizeBox = true;
+            this.metroSetControlBox1.MinimizeHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.metroSetControlBox1.MinimizeHoverForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.MinimizeNormalForeColor = System.Drawing.Color.Gray;
+            this.metroSetControlBox1.Name = "metroSetControlBox1";
+            this.metroSetControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.metroSetControlBox1.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetControlBox1.StyleManager = this.styleManager1;
+            this.metroSetControlBox1.TabIndex = 2;
+            this.metroSetControlBox1.Text = "metroSetControlBox1";
+            this.metroSetControlBox1.ThemeAuthor = null;
+            this.metroSetControlBox1.ThemeName = null;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoCompleteCustomSource = null;
+            this.lblUsername.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.lblUsername.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.lblUsername.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.lblUsername.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.lblUsername.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.lblUsername.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblUsername.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.lblUsername.Image = null;
+            this.lblUsername.Lines = null;
+            this.lblUsername.Location = new System.Drawing.Point(15, 259);
+            this.lblUsername.MaxLength = 32767;
+            this.lblUsername.Multiline = false;
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.ReadOnly = false;
+            this.lblUsername.Size = new System.Drawing.Size(262, 25);
+            this.lblUsername.Style = MetroSet_UI.Design.Style.Light;
+            this.lblUsername.StyleManager = this.styleManager1;
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Chanel";
+            this.lblUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.lblUsername.ThemeAuthor = null;
+            this.lblUsername.ThemeName = null;
+            this.lblUsername.UseSystemPasswordChar = false;
+            this.lblUsername.WatermarkText = "";
+            // 
+            // metroSetLabel1
+            // 
+            this.metroSetLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.metroSetLabel1.Location = new System.Drawing.Point(91, 232);
+            this.metroSetLabel1.Name = "metroSetLabel1";
+            this.metroSetLabel1.Size = new System.Drawing.Size(118, 24);
+            this.metroSetLabel1.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetLabel1.StyleManager = this.styleManager1;
+            this.metroSetLabel1.TabIndex = 4;
+            this.metroSetLabel1.Text = "Username:";
+            this.metroSetLabel1.ThemeAuthor = null;
+            this.metroSetLabel1.ThemeName = null;
+            // 
+            // metroSetLabel2
+            // 
+            this.metroSetLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.metroSetLabel2.Location = new System.Drawing.Point(91, 289);
+            this.metroSetLabel2.Name = "metroSetLabel2";
+            this.metroSetLabel2.Size = new System.Drawing.Size(118, 24);
+            this.metroSetLabel2.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetLabel2.StyleManager = this.styleManager1;
+            this.metroSetLabel2.TabIndex = 5;
+            this.metroSetLabel2.Text = "Password:";
+            this.metroSetLabel2.ThemeAuthor = null;
+            this.metroSetLabel2.ThemeName = null;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoCompleteCustomSource = null;
+            this.lblPassword.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.lblPassword.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.lblPassword.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.lblPassword.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.lblPassword.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(155)))), ((int)(((byte)(155)))));
+            this.lblPassword.DisabledForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblPassword.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.lblPassword.Image = null;
+            this.lblPassword.Lines = null;
+            this.lblPassword.Location = new System.Drawing.Point(15, 316);
+            this.lblPassword.MaxLength = 32767;
+            this.lblPassword.Multiline = false;
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.ReadOnly = false;
+            this.lblPassword.Size = new System.Drawing.Size(262, 25);
+            this.lblPassword.Style = MetroSet_UI.Design.Style.Light;
+            this.lblPassword.StyleManager = this.styleManager1;
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "*********";
+            this.lblPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.lblPassword.ThemeAuthor = null;
+            this.lblPassword.ThemeName = null;
+            this.lblPassword.UseSystemPasswordChar = false;
+            this.lblPassword.WatermarkText = "";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.DisabledBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnLogin.DisabledBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnLogin.DisabledForeColor = System.Drawing.Color.Gray;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnLogin.HoverBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.btnLogin.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(207)))), ((int)(((byte)(255)))));
+            this.btnLogin.HoverTextColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(102, 356);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.NormalBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnLogin.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(177)))), ((int)(((byte)(225)))));
+            this.btnLogin.NormalTextColor = System.Drawing.Color.White;
+            this.btnLogin.PressBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.btnLogin.PressColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(147)))), ((int)(((byte)(195)))));
+            this.btnLogin.PressTextColor = System.Drawing.Color.White;
+            this.btnLogin.Size = new System.Drawing.Size(80, 37);
+            this.btnLogin.Style = MetroSet_UI.Design.Style.Light;
+            this.btnLogin.StyleManager = this.styleManager1;
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "LOG IN";
+            this.btnLogin.ThemeAuthor = null;
+            this.btnLogin.ThemeName = null;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // metroSetLink1
+            // 
+            this.metroSetLink1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.metroSetLink1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.metroSetLink1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.metroSetLink1.Location = new System.Drawing.Point(15, 412);
+            this.metroSetLink1.Name = "metroSetLink1";
+            this.metroSetLink1.Size = new System.Drawing.Size(264, 23);
+            this.metroSetLink1.Style = MetroSet_UI.Design.Style.Light;
+            this.metroSetLink1.StyleManager = this.styleManager1;
+            this.metroSetLink1.TabIndex = 8;
+            this.metroSetLink1.TabStop = true;
+            this.metroSetLink1.Text = "Not signed up yet? Click here to sign up";
+            this.metroSetLink1.ThemeAuthor = null;
+            this.metroSetLink1.ThemeName = null;
+            this.metroSetLink1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(157)))), ((int)(((byte)(205)))));
+            // 
+            // styleManager1
+            // 
+            this.styleManager1.CustomTheme = "C:\\Users\\henro\\AppData\\Roaming\\Microsoft\\Windows\\Templates\\ThemeFile.xml";
+            this.styleManager1.MetroForm = this;
+            this.styleManager1.Style = MetroSet_UI.Design.Style.Light;
+            this.styleManager1.ThemeAuthor = null;
+            this.styleManager1.ThemeName = null;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(292, 447);
+            this.Controls.Add(this.metroSetLink1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.metroSetLabel2);
+            this.Controls.Add(this.metroSetLabel1);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.metroSetControlBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StyleManager = this.styleManager1;
+            this.Text = "LOGIN";
+            this.ThemeAuthor = null;
+            this.ThemeName = null;
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroSet_UI.Controls.MetroSetControlBox metroSetControlBox1;
+        private MetroSet_UI.Controls.MetroSetTextBox lblUsername;
+        private MetroSet_UI.Controls.MetroSetLabel metroSetLabel1;
+        private MetroSet_UI.Controls.MetroSetLabel metroSetLabel2;
+        private MetroSet_UI.Controls.MetroSetTextBox lblPassword;
+        private MetroSet_UI.Controls.MetroSetButton btnLogin;
+        private MetroSet_UI.Controls.MetroSetLink metroSetLink1;
+        private MetroSet_UI.StyleManager styleManager1;
+    }
+}
---
+++ Agile_Extension/Agile_Extension/Forms/frmLogin.cs
@@ -0,0 +1,32 @@
+﻿using MetroSet_UI.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agile_Extension.Forms
+{
+    public partial class frmLogin : MetroSetForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
