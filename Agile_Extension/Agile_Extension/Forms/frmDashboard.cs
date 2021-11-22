5f2d9e9be05d42d54ff274fa4b1385c464201b41
Neil Foxcroft <neilfoxcroftpro@gmail.com>
2021-11-22 15:54:10 +0200

    added GitIgnore to handle merges

--- Agile_Extension/Agile_Extension/Forms/frmDashboard.cs
+++ Agile_Extension/Agile_Extension/Forms/frmDashboard.cs
@@ -31,6 +31,7 @@ namespace Agile_Extension.Forms
                 btnAddProj.Enabled = true;
             }
             generateTabPages(projects);
+            ///testing
         }
 
         private void metroSetEllipse1_Click(object sender, EventArgs e)
